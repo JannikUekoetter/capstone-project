682ddecdab88b39bf2acdd8d3dd91fba747cca02
Jannik Uekötter <101580574+JannikUekoetter@users.noreply.github.com>
2022-10-19 15:06:37 +0200

    Revert "Add functional searchbar for both lost and found department of the app."

---
+++ components/SearchbarLostItem.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function SearchbarLostItem() {
+  return (
+    <div>
+      <h4>Search</h4>
+      <input placeholder="What are you looking for?"></input>
+    </div>
+  );
+}
