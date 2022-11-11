e466a863fd17046db7692f1a3c39f1194b06a97d
Jannik Uekoetter <uekoetter@protonmail.com>
2022-11-11 14:48:10 +0100

    last changes

--- components/Map.js
+++ components/Map.js
@@ -266,11 +266,13 @@ function LocationMarker() {
     )
   );
 }
-
 export default function Map() {
   return (
     <StyledMapContainer center={[51.235095, 9.682075]} zoom={6} scrollWheelZoom>
-      <TileLayer attribution='&copy; <a href=""></a> ' url="" />
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
 
       {positions.map((position) => {
         return (
